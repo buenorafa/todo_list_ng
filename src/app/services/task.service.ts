98c0cdf71ab28af4079b31b98bd66614f83fc579
buenorafa <rafaelbuenooo@outlook.com>
2024-08-08 00:36:31 -0300

    feat: add services to src/app/services/

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task'; // Ajuste o caminho conforme necessário
+import { DATA } from '../shared/data'; // Ajuste o caminho conforme necessário
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private tasks = DATA;
+
+  constructor() {}
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string, status: string): void {
+    let task = new Task(title, description, status);
+    this.tasks.push(task);
+  }
+
+  //   updateTask(updatedTask: Task): void {
+  //     const index = this.tasks.findIndex((task) => task.id === updatedTask.id);
+  //     if (index > -1) {
+  //       this.tasks[index] = updatedTask;
+  //     }
+  //   }
+
+  deleteTask(taskId: number): void {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+}
